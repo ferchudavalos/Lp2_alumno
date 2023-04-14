69f79ebb7dee6c1af51a7ffdd5f55d172de904b9
ferchudavalos <128001392+ferchudavalos@users.noreply.github.com>
2023-04-14 15:35:13 -0400

    Add files via upload

---
+++ materia/Materia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.materia;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Materia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
